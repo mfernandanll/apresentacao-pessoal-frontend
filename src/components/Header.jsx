95e269b1d97663e9d1314afab9a6bf20ea3eb9ce
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-10-11 15:01:20 -0400

    feat: implemented hamburger menu for mobile devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import { List, X } from "phosphor-react";
+import { useState } from "react";
 import { Link } from "react-scroll";
 
 export function Header(isPageScrolled = false) {
   const navLinkStyle = "hover:text-bg-text transition-all cursor-pointer";
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header
@@ -15,7 +23,9 @@ export function Header(isPageScrolled = false) {
         className="
           container flex justify-between items-center 
           mx-auto w-full 
-          max-w-[1320px] max-md:max-w-[960px] max-sm:max-w-[720px]"
+          max-w-[1320px] 
+          max-md:max-w-[960px] max-md:px-2
+          max-sm:max-w-[720px] px-6"
       >
         <Link
           to="hero"
@@ -31,7 +41,24 @@ export function Header(isPageScrolled = false) {
         </Link>
 
         <nav>
-          <ul className="flex gap-2">
+          <div 
+            onClick={toggleMenu}
+            className="hidden justify-between items-center p-4 max-lg:flex">
+            {
+              isOpen ?
+              <X size={24}/> :
+              <List size={24}/> 
+            }
+          </div>
+
+          <ul 
+            className={`
+              list-none flex gap-4 
+              lg:flex-row lg:static lg:p-0 max-lg:bg-bg-gradient-primary
+              transition-all ease-in-out duration-700
+              ${isOpen ? 'top-0' : '-top-[560%]'} absolute left-0 w-full bg-background200 flex-col p-20 z-[-1] max-lg:visible
+            `}
+          >
             <li>
               <Link
                 to="hero"
@@ -79,7 +106,7 @@ export function Header(isPageScrolled = false) {
                 Projetos
               </Link>
             </li>
-            
+
             <li>
               <Link
                 to="contact"
